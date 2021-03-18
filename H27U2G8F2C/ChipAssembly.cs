209d65ae5447638d078695ee1be60990fddc2ea0
Taras <taraskit@gmail.com>
2021-03-18 19:40:59 +0200

    add VSP config

--- H27U2G8F2C/ChipAssembly.cs
+++ H27U2G8F2C/ChipAssembly.cs
@@ -14,15 +14,23 @@ namespace H27U2G8F2C
     for automatically include <some code> in the READMY.md file in the repository
     */
 
-    #region
+  
     public class ChipAssembly
     {
         [Export("Chip")]
         ChipPrototype myChip = new ChipPrototype();
-        #endregion
+
 
 
         #region Chip parameters
+
+        //--------------------Vendor Specific Pin configuration---------------------------
+
+        //  VSP1(38pin) - NC    
+        //  VSP2(35pin) - NC
+        //  VSP3(20pin) - NC
+
+
 
         ChipAssembly()
         {
