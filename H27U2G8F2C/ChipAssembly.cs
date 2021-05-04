d33e371af62d5d53b4cb5e3dc76e36db24f54a17
JuliProg <taraskit@gmail.com>
2021-05-04 14:46:02 +0300

    Upadate CreateNewChip.yml and Add EccBits field

--- H27U2G8F2C/ChipAssembly.cs
+++ H27U2G8F2C/ChipAssembly.cs
@@ -14,11 +14,17 @@ namespace H27U2G8F2C
     for automatically include <some code> in the READMY.md file in the repository
     */
 
-  
+
+    public class ChipPrototype_v1 : ChipPrototype
+    {
+        public int EccBits;
+    }
+
+
     public class ChipAssembly
     {
         [Export("Chip")]
-        ChipPrototype myChip = new ChipPrototype();
+        ChipPrototype myChip = new ChipPrototype_v1();
 
 
 
@@ -47,8 +53,9 @@ namespace H27U2G8F2C
             myChip.colAdrCycles = 2;           // cycles for column addressing
             myChip.rowAdrCycles = 3;           // cycles for row addressing 
             myChip.vcc = Vcc.v3_3;             // supply voltage
+            (myChip as ChipPrototype_v1).EccBits = 1;                // required Ecc bits for each 512 bytes
 
-        #endregion
+            #endregion
 
 
             #region Chip operations
